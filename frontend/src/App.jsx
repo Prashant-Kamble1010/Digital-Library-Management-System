27cf88c150f35f087a5405763f6633c50c1ab7ea
Prashant Kamble <prashant.kamble24@pccoepune.org>
2026-09-08 19:17:36 +0530

    app.jsx remove error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,15 @@ import UpdateBook from "./UpdateBook";
 import Login from "./Login";
 import AddBook from "./AddBook";
 import UpdateBook from "./UpdateBook";
+
 function App() {
   return (
-    <Login />
-    <AddBook />
-    <UpdateBook />
+    <>
+      <Login />
+      <AddBook />
+      <UpdateBook />
+    </>
+
   );
 }
 
